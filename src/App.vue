a8b497bc8e5e60670633b2334d4d8671037987a0
vitorgil8 <ve3792682@gmail.com>
2025-07-18 11:35:00 -0300

    FEAT-2 ; inicio do html

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,141 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <header>
+      <nav>
+        <img src="../public/imagens/Captura de tela de 2025-07-04 13-40-50-Photoroom 1.png "
+          alt="tela de 2025-07-04 13-40-50-Photoroom.png">
+      </nav>
+      <ul>
+        <li>
+          <p>
+            vagas
+          </p>
+        </li>
+        <li>
+          Empresas
+        </li>
+        <li>
+          <button>
+            <p>
+              Login
+            </p>
+          </button>
+        </li>
+      </ul>
+    </header>
+    <section>
+      <p>
+        Oi, somos o projeto IMT, um site com o proposito de apoiar ao egressados na sua procura no ambito laboral e
+        empresas na busqueda de trabalhadores de qualidade, se tua empresa está procurando vacantes ou voce é um
+        egressado que comecou no mundo do trabalho, faça o login e os ajudaremos na sua procura.
+      </p>
+      <p>
+        Voce é:
+      </p>
+      <button>
+        <p>
+          Empresa
+        </p>
+      </button>
+      <p>
+        Ou
+      </p>
+      <button>
+        Formado
+      </button>
+      <img src="../public/imagens/image 11(2).png" alt="image 11.png">
+    </section>
+    <section>
+      <ul>
+        <li>
+          <img src="../public/imagens/image 1(2).png" alt="image 1(1).png">
+          <p>
+            Nossas estadisticas tem um avanço do 90%
+          </p>
+        </li>
+        <li>
+          <img src="../public/imagens/image 2(1).png" alt="image 2.png">
+          <p>
+            O 89% dos nossos graduados recebem propostas de trabalho cada semana
+          </p>
+        </li>
+        <li>
+          <img src="../public/imagens/image 3(1).png" alt="image 3.png">
+          <p>
+            O 94% das nossas empresas amigas se sentem satisfeitas com nossos graduados
+          </p>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <p>
+        No nosso site, a segurança dos seus dados pessoais é nossa prioridade. Utilizamos tecnologia de ponta para
+        proteger suas informações contra acessos não autorizados. Cada passo é cuidadosamente monitorado, garantindo que
+        sua privacidade seja sempre respeitada. Confie em nós para proporcionar uma experiência online segura e
+        tranquila. Aqui, seu bem-estar digital é o nosso compromisso!
+      </p>
+      <img src="../public/imagens/image 4(1).png" alt="image 4.png">
+    </section>
+    <section>
+      <img src="../public/imagens/image 5(1).png" alt="image 5.png">
+      <p>
+        Conectar-se com pessoas que podem ajudar é vital no ambiente laboral. Cada relacionamento é uma oportunidade de
+        aprendizado e crescimento. Ao cultivar redes, você não só expande seu conhecimento, mas também encontra apoio e
+        inspiração para alcançar seus objetivos profissionais.
+      </p>
+      <p>
+        Para alcançar o sucesso profissional, é essencial adquirir habilidades específicas por meio de educação contínua
+        e capacitação. Dominar conhecimentos técnicos e desenvolver competências interpessoais são passos fundamentais
+        para conquistar um posto de destaque no mercado de trabalho.
+      </p>
+      <img src="../public/imagens/image 6(1).png" alt="image 6.png">
+    </section>
   </main>
+  <footer>
+    <img src="../public/imagens/Captura de tela de 2025-07-04 13-40-50-Photoroom 1.png"
+      alt="Captura de tela de 2025-07-04 13-40-50-Photoroom.png">
+    <ul>
+      <li>
+        <p>
+          geral
+        </p>
+      </li>
+      <li>
+        <p>
+          login
+        </p>
+      </li>
+      <li>
+        <p>
+          Empresas
+        </p>
+      </li>
+      <li>
+        <p>
+          Vagas
+        </p>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.<path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+      </li>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.<path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
+      </li>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.<path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+      </li>
+    </ul>
+    <p>&copy;2025 Copyright</p>
+  </footer>
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
