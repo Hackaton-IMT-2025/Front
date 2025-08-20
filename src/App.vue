580777e1a80134a50b1f66c833be38859bcf6f38
pedrovieira08 <fafnir.pgv@gmail.com>
2025-08-20 09:14:58 -0300

    mainpage

--- src/App.vue
+++ src/App.vue
@@ -1,175 +1,138 @@
 <script setup>
-
 </script>
 
 <template>
-  <main>
-    <header>
-      <nav>
-        <img src="../public/imagens/Captura de tela de 2025-07-04 13-40-50-Photoroom 1.png "
-          alt="tela de 2025-07-04 13-40-50-Photoroom.png">
-      </nav>
-      <ul>
-        <li>
-          <p>
-            <a href="#">vagas</a>
-          </p>
-        </li>
-        <li>
-          <a href="#">Empresas</a>
-        </li>
-        <li>
-          <button>
-            <p class="login">
-              <a href="#">Login</a>
-            </p>
-          </button>
-        </li>
-      </ul>
-    </header>
-    <section class="introducao">
-      <p>
-        Oi, somos o projeto IMT, um site com o proposito de apoiar ao egressados na sua procura no ambito laboral e
-        empresas na busqueda de trabalhadores de qualidade, se tua empresa está procurando vacantes ou voce é um
-        egressado que comecou no mundo do trabalho, faça o login e os ajudaremos na sua procura.
-      </p>
-      <div>
-      <p>
-        Voce é:
-      </p>
-      <button>
-          Empresa
-      </button>
-      <p>
-        Ou
-      </p>
-      <button>
-        Formado
-      </button>
-      </div>
-    </section>
-    <section>
-      <ul>
-        <li>
-          <img src="../public/imagens/image 1(2).png" alt="image 1(1).png">
-          <p>
-            Nossas estadisticas tem um avanço do 90%
-          </p>
-        </li>
-        <li>
-          <img src="../public/imagens/image 2(1).png" alt="image 2.png">
-          <p>
-            O 89% dos nossos graduados recebem propostas de trabalho cada semana
-          </p>
-        </li>
-        <li>
-          <img src="../public/imagens/image 3(1).png" alt="image 3.png">
-          <p>
-            O 94% das nossas empresas amigas se sentem satisfeitas com nossos graduados
-          </p>
-        </li>
-      </ul>
-    </section>
-    <section>
-      <p>
-        No nosso site, a segurança dos seus dados pessoais é nossa prioridade. Utilizamos tecnologia de ponta para
-        proteger suas informações contra acessos não autorizados. Cada passo é cuidadosamente monitorado, garantindo que
-        sua privacidade seja sempre respeitada. Confie em nós para proporcionar uma experiência online segura e
-        tranquila. Aqui, seu bem-estar digital é o nosso compromisso!
-      </p>
-      <img src="../public/imagens/image 4(1).png" alt="image 4.png">
-    </section>
-    <section>
-      <img src="../public/imagens/image 5(1).png" alt="image 5.png">
-      <p>
-        Conectar-se com pessoas que podem ajudar é vital no ambiente laboral. Cada relacionamento é uma oportunidade de
-        aprendizado e crescimento. Ao cultivar redes, você não só expande seu conhecimento, mas também encontra apoio e
-        inspiração para alcançar seus objetivos profissionais.
-      </p>
-      <p>
-        Para alcançar o sucesso profissional, é essencial adquirir habilidades específicas por meio de educação contínua
-        e capacitação. Dominar conhecimentos técnicos e desenvolver competências interpessoais são passos fundamentais
-        para conquistar um posto de destaque no mercado de trabalho.
-      </p>
-      <img src="../public/imagens/image 6(1).png" alt="image 6.png">
-    </section>
-  </main>
-  <footer>
-    <img src="../public/imagens/Captura de tela de 2025-07-04 13-40-50-Photoroom 1.png"
-      alt="Captura de tela de 2025-07-04 13-40-50-Photoroom.png">
-    <ul>
-      <li>
-        <p>
-          geral
-        </p>
-      </li>
-      <li>
-        <p>
-          login
-        </p>
-      </li>
-      <li>
-        <p>
-          Empresas
-        </p>
-      </li>
-      <li>
-        <p>
-          Vagas
-        </p>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.<path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
-      </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.<path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
-      </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.<path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
-      </li>
-    </ul>
-    <p>&copy;2025 Copyright</p>
-  </footer>
-</template>
+  <!--fundo do site-->
+  <div class="background">
+    <div class="degrade-top"></div>
 
+    <div class="lado-esquerdo"></div>
+    <div class="lado-direito"></div>
+    <main class="mainpage">
+      <section class="hero">
+        <!-- Menu -->
+        <div class="nav">
+          <a href="#">Vagas</a>
+          <a href="#">Empresas</a>
+          <button>Login</button>
+        </div>
+
+        <!-- Texto principal -->
+        <div class="intro-text">
+          <p>
+            Oi, somos o projeto IMT, um site com o propósito de apoiar os egressados na sua procura
+            no âmbito laboral e empresas na busca de trabalhadores de qualidade. Se tua empresa está
+            procurando vagas ou você é um egresso que começou no mundo do trabalho, faça o login e
+            os ajudaremos na sua procura.
+          </p>
+          <p class="voce">Você é:</p>
+        </div>
+
+        <!-- Botões -->
+        <div class="botoes-opcao">
+          <button>Empresa</button>
+          <button>Formado</button>
+        </div>
+
+        <!-- Título -->
+        <h1>
+          INTRODUÇÃO AO<br />
+          MUNDO DO TRABALHO
+        </h1>
+
+        <!-- Seta -->
+        <div class="scroll-down">ROLE A PÁGINA</div>
+      </section>
+    </main>
+
+    <div class="degrade-bottom"></div>
+  </div>
+</template>
+ 
 
 <style scoped>
+/*==============================================
+                  background                    
+===============================================*/
+.background {
+  background: #05030b;
+  display: flex;
+  height: 100vh;
+  width: 100%;
+}
+.degrade-top,
+.degrade-bottom {
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 150px;
+  z-index: 5;
+  pointer-events: none;
+}
 
-header{
-  display: flex;
-  justify-content: space-between;
-  margin:30px 110px 30px 100px;
-  
-}header ul{
-  display: flex;
+.degrade-top {
+  top: 0;
+  background: linear-gradient(to bottom, #08081E, rgba(8, 8, 30, 0));
+}
+
+.degrade-bottom {
+  bottom: 0;
+  background: linear-gradient(to top, #08081E, rgba(8, 8, 30, 0));
+}
+
+.lado-esquerdo {
+  width: 50%;
+  background: conic-gradient(from 90deg at 50% 80%, #0c0c0c 0deg, #B1B1B1 110deg, #f8f8f8 360deg);
+  mix-blend-mode: color-dodge;
+  height: 100%;
+}
+
+.lado-direito {
+  width: 50%;
+  background: conic-gradient(from 90deg at 50% 80%, #0c0c0c 0deg, #B1B1B1 110deg, #f8f8f8 360deg);
+  mix-blend-mode: color-dodge;
+  height: 100%;
+  transform: scaleX(-1);
+}
+/*======================================
+                mainpage
+=======================================*/
+.mainpage {
+  position: absolute;
+  align-content: center;
   align-items: center;
+}.hero{
+  align-items: center;
+  text-align:center ;
+}.nav{
+margin: 50px 0px 0px 60vw;
+align-content: start;
 
-}header ul li{
-  padding: 0px 20px ;
-  
-}header ul li a{
- text-decoration: none;
- color: #fff;
+}a{
+ color: white;
  font-size: 20px;
-}header ul li p.login a{
-  color: black;
-  padding: 10px;
-  border-radius: 80%;
-}
-/*=====================================
-               introdução
-======================================*/
-section.introducao p{
-  align-self: center;
-  text-align: center;
-  padding: 20px 500px;
-  font-size: 17px;
-}section.introducao div{
-   display: flex;
-   align-items: center;
-}
+ text-decoration: none;
+ padding: 0px 40px;
+ color-interpolation-filters: sRGB;
+ 
+}p{
+  color: #f8f8f8;
+  margin: 40px 32vw 0px 32vw;
+  line-height: 23px;
+}button{
+  padding: 10px 20px;
+  border-radius: 20px;
+  border-style: none;
+  margin: 0px 0px 0px 25px;
+}.botoes-opcao button{
+  margin: 10px 60px;
+}h1{
+  font-size: 6vw;
+  color: #b1b1b1;
+  mix-blend-mode: color-dodge;
+  margin: 10px;
 
-
+}.voce{
+  margin: 10px;
+}
 </style>
