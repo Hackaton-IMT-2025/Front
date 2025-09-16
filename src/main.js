b55884ce84af7d424b4c3978551141ac7b7fa37a
Lucidrey <Lucidreymaitab@gmail.com>
2025-09-16 16:30:40 -0300

    Dev: Começando a pasta: views, stores, api e router

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+
+const app = createApp(App)
 
 createApp(App).mount('#app')
+
+app.use(createPinia())
+app.mount('#app')
